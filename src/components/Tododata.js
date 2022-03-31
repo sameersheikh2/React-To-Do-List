41f03f40f0865974e71f0b3a51d5ef71a9c2eacb
sameersheikh2 <sameersheih819@gmail.com>
2022-03-31 18:14:17 +0530

    to-do-list data added as array

---
+++ src/components/Tododata.js
@@ -0,0 +1,29 @@
+const tododata = [
+  {
+    id: 1,
+    text: "Get up early in the morning.",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Clean the room",
+    completed: true,
+  },
+  {
+    id: 3,
+    text: "Go for shopping",
+    completed: true,
+  },
+  {
+    id: 4,
+    text: "Eat healthy food",
+    completed: true,
+  },
+  {
+    id: 5,
+    text: "",
+    completed: true,
+  },
+]
+
+export default tododata;

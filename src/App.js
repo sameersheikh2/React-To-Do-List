09f37e72c923eebea424202cb43945bc49e45823
sameersheikh2 <sameersheih819@gmail.com>
2022-04-04 13:21:17 +0530

    created input and list where task will be shown

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ import Todoitem from "./components/Todoitem";
 function App() {
 
   return (
-    <div className="App">
+  <div className="App-background">
+    <p className="heading-text">My react to do</p>
+    <div className="container">
+      <div>
+        <input type="text"></input>
+        <button className="add-button" >Add</button>
+      </div>
+      <div>
+        <p className="no-item-text">No task added !</p>
+        <Todoitem />
+      </div>
     </div>
+  </div>
   );
 }
 

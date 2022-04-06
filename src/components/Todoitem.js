a3c499eeb76dd0082644a53071c93b497450a309
sameersheikh2 <sameersheih819@gmail.com>
2022-04-06 12:09:14 +0530

    added some css

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import Delete from './Delete.png'
 
 function Todoitem(props) {
 
 return (
-    <div className='todo-list'>
-     
-    </div>
-  )
+  <div className="todo-list jc-space row column">
+    <span className={props.item.isComplete?"task-done":""} onClick={() => props.markAsDone(props.index)}>{props.item.description}</span>
+    <img
+      src={Delete}
+      alt=""
+      className="icon"
+      onClick={() => props.deleteTask(props.index)}
+    />
+  </div>
+);
 }
 
 export default Todoitem;
